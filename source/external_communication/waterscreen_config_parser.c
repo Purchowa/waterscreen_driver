bece68aa3bafbad6ff1b4fde1c0f2c0a37e75492
Dawid_W <purchowa@gmail.com>
2024-09-05 21:29:36 +0200

    add: additional test-case for config parser Clamping what can be clamped

--- source/external_communication/waterscreen_config_parser.c
+++ source/external_communication/waterscreen_config_parser.c
@@ -2,6 +2,15 @@
 
 #include "cjson/cjson.h"
 
+#define MAX_IDLE_TIME 30
+#define MAX_WORK_TIME 30
+
+static uint16_t clamp_uint16( const int32_t value, const uint16_t min, const uint16_t max )
+{
+    const uint16_t lower = value < min ? min : value;
+    return max < lower ? max : lower;
+}
+
 static bool parseJsonConfig( const cJSON *cfgJson, WaterscreenConfig_t *config )
 {
     if ( cfgJson == NULL )
@@ -18,24 +27,23 @@ static bool parseJsonConfig( const cJSON *cfgJson, WaterscreenConfig_t *config )
     config->isWorkingDuringWeekends = enableWeekends->valueint;
 
     const cJSON *workTime = cJSON_GetObjectItemCaseSensitive( cfgJson, "workTime" );
-    if ( !cJSON_IsNumber( workTime ) || workTime->valueint < 0 )
+    if ( !cJSON_IsNumber( workTime ) )
         return false;
-    config->workTimeInStandardMode = workTime->valueint;
+    config->workTimeInStandardMode = clamp_uint16( workTime->valueint, 0, MAX_WORK_TIME );
 
     const cJSON *idleTime = cJSON_GetObjectItemCaseSensitive( cfgJson, "idleTime" );
-    if ( !cJSON_IsNumber( idleTime ) || idleTime->valueint < 0 )
+    if ( !cJSON_IsNumber( idleTime ) )
         return false;
-    config->idleTimeInStandardMode = idleTime->valueint;
+    config->idleTimeInStandardMode = clamp_uint16( idleTime->valueint, 0, MAX_IDLE_TIME );
 
     const cJSON *pictureObject = cJSON_GetObjectItemCaseSensitive( cfgJson, "picture" );
     if ( !cJSON_IsObject( pictureObject ) )
         return false;
 
     const cJSON *pictureSize = cJSON_GetObjectItemCaseSensitive( pictureObject, "size" );
-    if ( !cJSON_IsNumber( pictureSize ) ||
-         ( pictureSize->valueint < 0 || MAX_CUSTOM_PICTURE_LENGTH < pictureSize->valueint ) )
+    if ( !cJSON_IsNumber( pictureSize ) )
         return false;
-    config->customPictureSize = pictureSize->valueint;
+    config->customPictureSize = clamp_uint16( pictureSize->valueint, 0, MAX_CUSTOM_PICTURE_LENGTH );
 
     const cJSON *pictureArray = cJSON_GetObjectItemCaseSensitive( pictureObject, "data" );
     if ( !cJSON_IsArray( pictureArray ) )
@@ -45,11 +53,17 @@ static bool parseJsonConfig( const cJSON *cfgJson, WaterscreenConfig_t *config )
     uint16_t     i       = 0;
     cJSON_ArrayForEach( element, pictureArray )
     {
-        if ( !cJSON_IsNumber( element ) || config->customPictureSize <= i )
+        if ( !cJSON_IsNumber( element ) )
             return false;
+
+        if ( config->customPictureSize <= i )
+            break;
 
         config->customPicture[i++] = element->valueint;
     }
+
+    if ( i < config->customPictureSize )
+        return false;
 
     return true;
 }
--- tests/src/config_parser_test.c
+++ tests/src/config_parser_test.c
@@ -24,7 +24,7 @@ static bool compareConfigs( const WaterscreenConfig_t *first, const WaterscreenC
     return areEqual;
 }
 
-void givenRawValidJson_configParser_fillConfigStructure( void ** )
+void givenRawValidJson_configParser_fillConfigStructure()
 {
 
     const char *sampleJson = "{\"mode\":2,\"enableWeekends\":true,\"workTime\":10,\"idleTime\":5,\"picture\":{\"size\":"
@@ -41,7 +41,7 @@ void givenRawValidJson_configParser_fillConfigStructure( void ** )
     assert_true( compareConfigs( &expectedConfig, &config ) );
 }
 
-void givenInvlaidMode_configParser_returnFalse( void ** )
+void givenInvlaidMode_configParser_returnFalse()
 {
     WaterscreenConfig_t config;
 
@@ -56,26 +56,53 @@ void givenInvlaidMode_configParser_returnFalse( void ** )
     assert_false( fromJsonToWaterscreenCfg( invlaidMode_higher, &config ) );
 }
 
-void givenInvalidSize_configParser_returnFalse( void ** )
+void givenArrayShorterThanSize_configParser_returnFalse()
 {
     WaterscreenConfig_t config;
+
+    const char *invalidArrayWithSize =
+        "{\"mode\":0,\"enableWeekends\":true,\"workTime\":0,\"idleTime\":0,\"picture\":{\"size\":"
+        "4,\"data\":[0,0,24]}}";
+    assert_false( fromJsonToWaterscreenCfg( invalidArrayWithSize, &config ) );
+}
+
+void givenInvalidSize_configParser_returnTrueAndClamp()
+{
+    WaterscreenConfig_t configLower;
 
     const char *invlaidSize_lower =
         "{\"mode\":2,\"enableWeekends\":true,\"workTime\":10,\"idleTime\":5,\"picture\":{\"size\":"
         "-1,\"data\":[0,0,24,1567]}}";
-    assert_false( fromJsonToWaterscreenCfg( invlaidSize_lower, &config ) );
+    WaterscreenConfig_t expectedConfig = { .mode                    = Service,
+                                           .isWorkingDuringWeekends = true,
+                                           .workTimeInStandardMode  = 10,
+                                           .idleTimeInStandardMode  = 5,
+                                           .customPictureSize       = 0,
+                                           .customPicture           = { 0, 0, 24, 1567 } };
 
-    const char *invlaidSize_higher =
+    assert_true( fromJsonToWaterscreenCfg( invlaidSize_lower, &configLower ) );
+    assert_true( compareConfigs( &expectedConfig, &configLower ) );
+
+    WaterscreenConfig_t configHigher;
+    const char         *invlaidSize_higher =
         "{\"mode\":2,\"enableWeekends\":true,\"workTime\":10,\"idleTime\":5,\"picture\":{\"size\":"
-        "65,\"data\":[0,0,24,1567]}}";
-    assert_false( fromJsonToWaterscreenCfg( invlaidSize_higher, &config ) );
+        "65,\"data\":[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,"
+        "0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]}}";
+
+    expectedConfig.customPictureSize = MAX_CUSTOM_PICTURE_LENGTH;
+    memset( expectedConfig.customPicture, 0,
+            sizeof( *expectedConfig.customPicture ) * expectedConfig.customPictureSize );
+
+    assert_true( fromJsonToWaterscreenCfg( invlaidSize_higher, &configHigher ) );
+    assert_true( compareConfigs( &expectedConfig, &configHigher ) );
 }
 
 int main()
 {
     const struct CMUnitTest tests[] = { cmocka_unit_test( givenRawValidJson_configParser_fillConfigStructure ),
                                         cmocka_unit_test( givenInvlaidMode_configParser_returnFalse ),
-                                        cmocka_unit_test( givenInvalidSize_configParser_returnFalse ) };
+                                        cmocka_unit_test( givenArrayShorterThanSize_configParser_returnFalse ),
+                                        cmocka_unit_test( givenInvalidSize_configParser_returnTrueAndClamp ) };
 
     return cmocka_run_group_tests_name( "JSON config parser", tests, NULL, NULL );
 }
